0728dfaf073e6e4ff74317ef2e168869ff1ca358
skyharbor <dominiknikiel@gmail.com>
2022-10-13 11:36:44 +0200

    Update client cache - disable merge on interactions.

--- client/src/client.ts
+++ client/src/client.ts
@@ -3,7 +3,17 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 // Instance of Apollo Client
 const client = new ApolloClient({
   uri: "http://localhost:4000",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          interactions: {
+            merge: false,
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default client;
